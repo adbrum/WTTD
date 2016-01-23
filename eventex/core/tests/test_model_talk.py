520aef56b65285663bd92a447aebf2183ac9fb78
adbrum <adbrum@outlook.com>
2016-01-23 01:02:18 +0000

    Managers

--- eventex/core/tests/test_model_talk.py
+++ eventex/core/tests/test_model_talk.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
+from eventex.core.managers import PeriodManager
 from eventex.core.models import Talk
 
 
 class TalkModelTest(TestCase):
     def setUp(self):
-        self. talk = Talk.objects.create(
+        self.talk = Talk.objects.create(
             title='Título da Palestra'
         )
 
@@ -23,19 +24,38 @@ class TalkModelTest(TestCase):
 
     def test_description_blank(self):
         field = Talk._meta.get_field('description')
-        self. assertTrue(field.blank)
+        self.assertTrue(field.blank)
 
     def test_speakers_blank(self):
         field = Talk._meta.get_field('speakers')
-        self. assertTrue(field.blank)
+        self.assertTrue(field.blank)
 
     def test_start_blank(self):
         field = Talk._meta.get_field('start')
-        self. assertTrue(field.blank)
+        self.assertTrue(field.blank)
 
     def test_start_null(self):
         field = Talk._meta.get_field('start')
-        self. assertTrue(field.null)
+        self.assertTrue(field.null)
 
     def test_str(self):
         self.assertEqual('Título da Palestra', str(self.talk))
+
+
+class PeriodManagerTest(TestCase):
+    def setUp(self):
+        Talk.objects.create(title='Morning Talk', start='11:59')
+        Talk.objects.create(title='Afternoon Talk', start='12:00')
+
+    def test_manager(self):
+        self.assertIsInstance(Talk.objects, PeriodManager)
+
+    def test_at_morning(self):
+        qs = Talk.objects.at_morning()
+        expected = ['Morning Talk']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.title)
+
+    def test_at_afternoon(self):
+        qs = Talk.objects.at_afternoon()
+        expected = ['Afternoon Talk']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.title)
